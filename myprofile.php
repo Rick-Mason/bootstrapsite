07199740bd3c15e38f6bafd4d7e4e80a1c124cfc
Rick-Mason <westondance@yahoo.com>
2015-01-29 05:11:15 -0500

    switched to one left join query and a couple of other queries, rather then 1 overall multitable select

--- myprofile.php
+++ myprofile.php
@@ -12,7 +12,13 @@ include_once( "lib/scripts.php" );
  * Pull all relavent data from the database in one query
  */
 
-$sql = "SELECT 	
+ 
+	
+
+
+
+
+/*$sql = "SELECT 	
 			user.user_name,
 			user.user_email,
 			user_description.user_description,
@@ -32,21 +38,63 @@ $sql = "SELECT
 		AND 
 			user.user_id = user_personal_info.user_id
 		AND 
-			user.user_id = user_image.user_id";
-var_dump( $sql );
+			user.user_id = user_image.user_id";*/
+
+// data from user and user_image in one joined query
+$sql = "SELECT 
+			user.user_id, 
+  			user.user_name,
+  			user.user_email, 
+   			user_image.image_name 
+		FROM 
+			user 
+		LEFT JOIN 
+			user_image 
+		ON 
+			user.user_id = user_image.user_id
+		WHERE 
+			user.user_id = {$_SESSION['user_id']}";
+
 $stmt = $dbh->query( $sql );
 $row = $stmt->fetch( PDO::FETCH_ASSOC );
-
-$sEmail = $row['user_email'];
+$sUserEmail = $row['user_email'];
 $sUserName = $row['user_name'];
-var_dump( $sUserName );
+
+if( $row['image_name'] ) {
+	$sImagePath = IMAGE_BASE_PATH . $row['image_name'];
+	$sImageString = wrapImageTag( $sImagePath );
+} else {
+	$sImageString = "<h3>No Image on File</h3>";
+}
 
 
+//data from user_description
+$sql = "SELECT 
+			user_description
+		FROM 
+			user_description
+		WHERE
+			user_id = {$_SESSION['user_id']}";
+$stmt = $dbh->query( $sql );
+$row  = $stmt->fetch( PDO::FETCH_ASSOC );
 if ( $row['user_description'] ) {
 	$sUserDescription = html_entity_decode( $row['user_description']);
 } else {
 	$sUserDescription = "<h3>No paragraph on file</h3>";
 }
+
+
+//data from user_personal_info
+$sql = "SELECT 
+			user_first,
+			user_last,
+			user_phone
+		FROM	
+			user_personal_info
+		WHERE
+			user_id = {$_SESSION['user_id']}";
+$stmt = $dbh->query( $sql );
+$row  = $stmt->fetch( PDO::FETCH_ASSOC );
 
 if ( $row['user_first'] ) {
 	$sUserFirstAndLast = ucfirst( $row['user_first']) 
@@ -55,19 +103,13 @@ if ( $row['user_first'] ) {
 } else {
 	$sUserFirstAndLast = "No First and Last name on file.";
 }
-
 if( $row['user_phone'] ) {
 	$sUserPhone = formatPhone( $row['user_phone'] );
 } else {
 	$sUserPhone = "No Phone number on file.";
 }
 
-if( $row['image_name'] ) {
-	$sImagePath = IMAGE_BASE_PATH . $row['image_name'];
-	$sImageString = wrapImageTag( $sImagePath );
-} else {
-	$sImageString = "<h3>No Image on File</h3>";
-}
+
 
 
 
